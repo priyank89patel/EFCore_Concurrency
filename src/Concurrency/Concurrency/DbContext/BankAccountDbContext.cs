9d982881dce22640150316c0da15b8f461426f75
Priyank Patel <priyank89patel@gmail.com>
2020-05-01 21:37:03 -0400

    Enable EF logging

--- src/Concurrency/Concurrency/DbContext/BankAccountDbContext.cs
+++ src/Concurrency/Concurrency/DbContext/BankAccountDbContext.cs
@@ -5,6 +5,8 @@ using System.Text;
 using Concurrency.DbContext.Configurations;
 using Concurrency.Models;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Console;
 
 namespace Concurrency.DbContext
 {
@@ -14,9 +16,22 @@ namespace Concurrency.DbContext
         public DbSet<ConcurrentAccountWithToken> ConcurrentAccountsWithToken { get; protected set; }
         public DbSet<NonConcurrentAccount> NonConcurrentAccounts { get; protected set; }
 
+        public static readonly ILoggerFactory EfLoggerFactory
+    = LoggerFactory.Create(builder =>
+    {
+        builder
+            .AddFilter((category, level) =>
+                 category == DbLoggerCategory.Database.Command.Name
+                    && level == LogLevel.Information)
+            .AddConsole()
+            .AddDebug();
+    });
+
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseSqlServer(
+            optionsBuilder
+                .UseLoggerFactory(EfLoggerFactory)
+                .UseSqlServer(
             @"Server=(localdb)\mssqllocaldb;Database=EFCoreConcurrency;ConnectRetryCount=0");
         }
 
